0b5b6523c02b0a0a861f7bb1d120170514056325
kavaleud <dzmitry.kavaleuski@viacomcontractor.com>
2017-07-07 16:12:33 -0400

    add fibonacci script

---
+++ fibonacci.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+fibonacci_length = ARGV[0]
+
+if fibonacci_length.nil?
+  puts "Argument missed! Please enter positive value"
+elsif fibonacci_length.to_i == 1
+  puts 0
+elsif fibonacci_length.to_i > 1
+	n = 1
+	fibonacci = [0, n]
+
+	while fibonacci.length < fibonacci_length.to_i
+	  fibonacci[n+1] = fibonacci[n] + fibonacci[n-1]
+	  n += 1
+	end
+
+	puts fibonacci.join(", ")
+  
+else
+  puts "Wrong argument: please enter positive value"
+
+end
+
+
+
+
+ 
+
+
