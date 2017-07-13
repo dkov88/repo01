15a429a52584712254b795fef333314834418889
kavaleud <dzmitry.kavaleuski@viacomcontractor.com>
2017-07-13 07:45:50 -0400

    fixed code style for mega_sum

--- mega_sum.rb
+++ mega_sum.rb
@@ -2,24 +2,24 @@
 
 def mega_sum(array, *args, &block)
 	
-	sum = 0
-	i = 0
+  sum = 0
+  i = 0
 	
-	while i < array.size
-		unless block_given?
-			sum = array.inject(0){|sum,x| sum + x }
-		else
-		  val = yield array[i]
-		  sum += val
-		end
-		i += 1
-	end
+  while i < array.size
+    unless block_given?
+      sum = array.inject(0){|sum,x| sum + x }
+    else
+      val = yield array[i]
+      sum += val
+    end
+    i += 1
+  end
   
   if args[0]
-  	sum = sum + args[0]
+    sum = sum + args[0]
   end
 
-	puts sum
+  puts sum
 end
 
 mega_sum([1, 2, 3, 4])
